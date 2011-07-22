10f4d61476b9afa7a50841e5401d548db70dd928
Brian Bauer <brian.bauer@g5platform.com>
2011-07-22 00:37:54 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Backpacks::Application.config.secret_token = '4b72756140cc6a4180672d79fe37171196d8bed2ff6cbd07fe13031182d3f115a27709a5951e494bc69ef8722676dbfd5ac296b03c4690f23c79ab04de56a12d'
